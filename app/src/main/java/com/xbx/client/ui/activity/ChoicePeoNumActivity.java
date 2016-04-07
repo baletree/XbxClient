4e4b0575fe63d875401d4942e34c226799248468
EricPeng <1209085180@qq.com>
2016-04-07 21:52:02 +0800

    人数选择

--- app/src/main/java/com/xbx/client/ui/activity/ChoicePeoNumActivity.java
+++ app/src/main/java/com/xbx/client/ui/activity/ChoicePeoNumActivity.java
@@ -3,20 +3,26 @@ package com.xbx.client.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.xbx.client.R;
+import com.xbx.client.utils.Util;
+import com.xbx.client.view.WheelView;
+
+import java.util.Arrays;
 
 /**
  * Created by EricYuan on 2016/4/5.
  */
 public class ChoicePeoNumActivity extends Activity implements View.OnClickListener{
 
-    private TextView guide_opeo_tv;
-    private TextView guide_fpeo_tv;
-    private TextView guide_speo_tv;
-    private TextView guide_tpeo_tv;
+    private TextView peo_cancel_txt;
+    private TextView peo_sure_txt;
+    private WheelView peonum_wheelView;
+
+    private String peopleNum[] = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,37 +33,37 @@ public class ChoicePeoNumActivity extends Activity implements View.OnClickListen
     }
 
     private void initViews(){
-        guide_opeo_tv = (TextView) findViewById(R.id.guide_opeo_tv);
-        guide_fpeo_tv = (TextView) findViewById(R.id.guide_fpeo_tv);
-        guide_speo_tv = (TextView) findViewById(R.id.guide_speo_tv);
-        guide_tpeo_tv = (TextView) findViewById(R.id.guide_tpeo_tv);
+        peopleNum = getResources().getStringArray(R.array.peopleNum);
+        peo_cancel_txt = (TextView) findViewById(R.id.peo_cancel_txt);
+        peo_sure_txt = (TextView) findViewById(R.id.peo_sure_txt);
+        peonum_wheelView = (WheelView) findViewById(R.id.peonum_wheelView);
+        peonum_wheelView.setOffset(1);
+        peonum_wheelView.setItems(Arrays.asList(peopleNum));
     }
 
     private void initLisener(){
-        guide_opeo_tv.setOnClickListener(this);
-        guide_fpeo_tv.setOnClickListener(this);
-        guide_speo_tv.setOnClickListener(this);
-        guide_tpeo_tv.setOnClickListener(this);
+        peo_cancel_txt.setOnClickListener(this);
+        peo_sure_txt.setOnClickListener(this);
+        peonum_wheelView.setOnWheelViewListener(new WheelView.OnWheelViewListener() {
+            @Override
+            public void onSelected(int selectedIndex, String item) {
+                super.onSelected(selectedIndex, item);
+                Log.i("Tag", "selectedIndex: " + selectedIndex + ", item: " + item);
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         Intent intent = new Intent();
         switch (v.getId()){
-            case R.id.guide_opeo_tv:
-                intent.putExtra("choiceType", 1);
+            case R.id.peo_cancel_txt:
+                finish();
                 break;
-            case R.id.guide_fpeo_tv:
-                intent.putExtra("choiceType", 2);
-                break;
-            case R.id.guide_speo_tv:
-                intent.putExtra("choiceType", 3);
-                break;
-            case R.id.guide_tpeo_tv:
-                intent.putExtra("choiceType", 4);
+            case R.id.peo_sure_txt:
+                setResult(RESULT_OK,intent);
+                finish();
                 break;
         }
-        setResult(RESULT_OK,intent);
-        finish();
     }
 }
